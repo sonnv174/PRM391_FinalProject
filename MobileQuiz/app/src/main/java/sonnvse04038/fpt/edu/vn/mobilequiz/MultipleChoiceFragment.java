b90969eb76e3835560b6ff81012e11a6e8bdfe5a
LeHoan <hoanltse03813@fpt.edu.vn>
2017-03-22 23:02:13 +0700

    Update Mul

--- MobileQuiz/app/src/main/java/sonnvse04038/fpt/edu/vn/mobilequiz/MultipleChoiceFragment.java
+++ MobileQuiz/app/src/main/java/sonnvse04038/fpt/edu/vn/mobilequiz/MultipleChoiceFragment.java
@@ -8,7 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import sonnvse04038.fpt.edu.vn.mobilequiz.Object.MultipleChoice;
 
 
 /**
@@ -17,6 +20,11 @@ import android.widget.Toast;
 public class MultipleChoiceFragment extends Fragment implements View.OnClickListener{
 
     RadioButton radA, radB, radC, radD, radE;
+    TextView tvQuestion;
+    String righAnswer = "Question A";
+    String answer = "";
+    boolean result = false;
+    MultipleChoice mc = new MultipleChoice();
 
     public MultipleChoiceFragment() {
         // Required empty public constructor
@@ -39,11 +47,29 @@ public class MultipleChoiceFragment extends Fragment implements View.OnClickList
         radD = (RadioButton) view.findViewById(R.id.radD);
         radE = (RadioButton) view.findViewById(R.id.radE);
 
+        tvQuestion = (TextView) view.findViewById(R.id.tvMulQuestion);
+
         radA.setOnClickListener(this);
         radB.setOnClickListener(this);
         radC.setOnClickListener(this);
         radD.setOnClickListener(this);
         radE.setOnClickListener(this);
+
+        mc.settID(1);
+        mc.setQuestion("Question");
+        mc.setAnswer1("Answer A");
+        mc.setAnswer2("Answer B");
+        mc.setAnswer3("Answer C");
+        mc.setAnswer4("Answer D");
+        mc.setAnswer5("Answer E");
+        mc.setRighAnswer(righAnswer);
+
+        tvQuestion.setText(mc.getQuestion());
+        radA.setText(mc.getAnswer1());
+        radB.setText(mc.getAnswer2());
+        radC.setText(mc.getAnswer3());
+        radD.setText(mc.getAnswer4());
+        radE.setText(mc.getAnswer5());
     }
 
     @Override
@@ -51,20 +77,28 @@ public class MultipleChoiceFragment extends Fragment implements View.OnClickList
         int GUI_id = view.getId();
         switch (GUI_id) {
             case R.id.radA:
-                Toast.makeText(getContext(), "A radio button is checked!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), radA.getText().toString()+" button is checked!", Toast.LENGTH_SHORT).show();
+                answer = radA.getText().toString();
                 break;
             case R.id.radB:
-                Toast.makeText(getContext(), "B radio button is checked!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), radB.getText().toString()+" button is checked!", Toast.LENGTH_SHORT).show();
+                answer = radB.getText().toString();
                 break;
             case R.id.radC:
-                Toast.makeText(getContext(), "C radio button is checked", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), radC.getText().toString()+" button is checked!", Toast.LENGTH_SHORT).show();
+                answer = radC.getText().toString();
                 break;
             case R.id.radD:
-                Toast.makeText(getContext(), "D radio button is checked!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), radD.getText().toString()+" button is checked!", Toast.LENGTH_SHORT).show();
+                answer = radD.getText().toString();
                 break;
             case R.id.radE:
-                Toast.makeText(getContext(), "E radio button is checked!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), radE.getText().toString()+" button is checked!", Toast.LENGTH_SHORT).show();
+                answer = radE.getText().toString();
                 break;
+        }
+        if(answer == mc.getRighAnswer()){
+            result = true;
         }
     }
 }
--- MobileQuiz/app/src/main/java/sonnvse04038/fpt/edu/vn/mobilequiz/Object/MultipleChoice.java
+++ MobileQuiz/app/src/main/java/sonnvse04038/fpt/edu/vn/mobilequiz/Object/MultipleChoice.java
@@ -5,4 +5,81 @@ package sonnvse04038.fpt.edu.vn.mobilequiz.Object;
  */
 
 public class MultipleChoice {
+    private String question, answer1, answer2, answer3, answer4, answer5, righAnswer;
+    private int tID;
+
+    public MultipleChoice() {
+        this.tID = tID;
+        this.question = question;
+        this.answer1 = answer1;
+        this.answer2 = answer2;
+        this.answer3 = answer3;
+        this.answer4 = answer4;
+        this.answer5 = answer5;
+        this.righAnswer = righAnswer;
+    }
+
+    public int gettID() {
+        return tID;
+    }
+
+    public void settID(int tID) {
+        this.tID = tID;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer1() {
+        return answer1;
+    }
+
+    public void setAnswer1(String answer1) {
+        this.answer1 = answer1;
+    }
+
+    public String getAnswer2() {
+        return answer2;
+    }
+
+    public void setAnswer2(String answer2) {
+        this.answer2 = answer2;
+    }
+
+    public String getAnswer3() {
+        return answer3;
+    }
+
+    public void setAnswer3(String answer3) {
+        this.answer3 = answer3;
+    }
+
+    public String getAnswer4() {
+        return answer4;
+    }
+
+    public void setAnswer4(String answer4) {
+        this.answer4 = answer4;
+    }
+
+    public String getAnswer5() {
+        return answer5;
+    }
+
+    public void setAnswer5(String answer5) {
+        this.answer5 = answer5;
+    }
+
+    public String getRighAnswer() {
+        return righAnswer;
+    }
+
+    public void setRighAnswer(String righAnswer) {
+        this.righAnswer = righAnswer;
+    }
 }
